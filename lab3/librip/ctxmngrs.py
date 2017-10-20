a00dd6bbee25aa977c3c2748ba678f8498d21d9a
kirillandreev8888 <kirillandreev8888@gmail.com>
2017-10-20 16:17:38 +0300

    added report 3

--- lab3/librip/ctxmngrs.py
+++ lab3/librip/ctxmngrs.py
@@ -10,11 +10,21 @@ import datetime
 import contextlib
 
 
-@contextlib.contextmanager
-def timer():
-    t1 = datetime.datetime.now()
-    yield
-    t2 = datetime.datetime.now()
-    res = t2 - t1
-    res1 = str(res.seconds) + '.' + str(res.microseconds)
-    print('Execution time '+res1)
+# @contextlib.contextmanager
+# def timer():
+#     t1 = datetime.datetime.now()
+#     yield
+#     t2 = datetime.datetime.now()
+#     res = t2 - t1
+#     res1 = str(res.seconds) + '.' + str(res.microseconds)
+#     print('Execution time '+res1)
+
+class timer:
+    t1 = 0
+    def __enter__(self):
+        self.t1 = datetime.datetime.now()
+    def __exit__(self, exp_type,exp_value, traceback):
+        t2 = datetime.datetime.now()
+        res = t2 - self.t1
+        res1 = str(res.seconds) + '.' + str(res.microseconds)
+        print('Execution time ' + res1)
